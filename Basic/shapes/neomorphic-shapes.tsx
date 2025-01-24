4374aa4337bef8e1d31d0c721e230626f70951e4
crazyAT8 <88wwatson@gmail.com>
2025-01-23 22:45:41 -0600

    1/23/25

---
+++ Basic/shapes/neomorphic-shapes.tsx
@@ -0,0 +1,16 @@
+export default function NeumorphicShapes() {
+    return (
+      <div className="min-h-screen bg-[#e6e9f0] flex items-center justify-center p-8">
+        <div className="flex items-center gap-8">
+          {/* First Square */}
+          <div className="w-16 h-16 bg-[#e6e9f0] rounded-lg shadow-[8px_8px_16px_0px_rgba(174,174,192,0.4),-8px_-8px_16px_0px_rgba(255,255,255,0.6)]"></div>
+  
+          {/* Second Square */}
+          <div className="w-16 h-16 bg-[#e6e9f0] rounded-lg shadow-[8px_8px_16px_0px_rgba(174,174,192,0.4),-8px_-8px_16px_0px_rgba(255,255,255,0.6)]"></div>
+  
+          {/* Circle */}
+          <div className="w-16 h-16 bg-[#e6e9f0] rounded-full shadow-[8px_8px_16px_0px_rgba(174,174,192,0.4),-8px_-8px_16px_0px_rgba(255,255,255,0.6)]"></div>
+        </div>
+      </div>
+    )
+  }
