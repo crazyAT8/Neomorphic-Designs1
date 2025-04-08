525495b3051680a98f8fe45fe6e7a61f39e27aca
crazyAT8 <88wwatson@gmail.com>
2025-04-07 23:44:23 -0500

    social media buttons

---
+++ Buttons/Social/script.js
@@ -0,0 +1,8 @@
+$('.first li').click(function(){
+    $(this).toggleClass("shadow-1").siblings();
+    $(this).toggleClass("fill-color").siblings();
+  });
+  $('.second li').click(function(){
+    $(this).toggleClass("shadow-2").siblings();
+    $(this).toggleClass("fill-color").siblings();
+  });
